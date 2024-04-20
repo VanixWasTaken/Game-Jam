43f931c5fd53fa2feca749caec358de8ee6aeca5
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-20 02:54:39 +0200

    level generation

--- Scenes and Scripts/level.gd
+++ Scenes and Scripts/level.gd
@@ -1,18 +1,23 @@
 extends Node2D
-#
-#var borders = Rect2(1, 1, 13, 7)
-#var ground_layer = 0
-#
-#@onready var tilemap = $TileMap
-#
-#func _ready():
-	#generate_level()
-#
-#func generate_level():
-	#var walker = Walker.new(Vector2(7, 4), borders)
-	#var map = walker.walk(500)
-	#walker.queue_free()
-	#for tile_pos in map:
-		#tilemap.set_cell(ground_layer, tile_pos, ground_layer, Vector2i(0, 3))
-		## Assuming used_cells is the same as map
-		#tilemap.update_bitmask_region(tile_pos, tile_pos + Vector2(1, 1))
+
+var tiles1 = preload("res://Scenes and Scripts/Tiles/tile_1.tscn")
+var tiles2 = preload("res://Scenes and Scripts/Tiles/tile_2.tscn")
+var last_tile_position = Vector2(0, 0)
+
+
+func _process(delta):
+	randomize()
+	var tiles = [tiles1, tiles2]
+	var help = tiles[randi()% tiles.size()]
+	var tile = help.instantiate()
+	add_child(tile)
+	tile.position = last_tile_position
+	last_tile_position += Vector2(1920, 0)
+	
+	
+	
+	if last_tile_position.x >= 57600:
+		last_tile_position.x = 0
+		last_tile_position += Vector2(0, 1080)
+
+	
