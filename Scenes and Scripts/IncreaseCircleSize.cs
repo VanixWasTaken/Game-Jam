87e9bace98141a66cdfe5790d53ea37abfd0613c
Liam <128694774+Iceisn1ce@users.noreply.github.com>
2024-04-20 14:35:34 +0200

    Game over if anim is over

--- Scenes and Scripts/IncreaseCircleSize.cs
+++ Scenes and Scripts/IncreaseCircleSize.cs
@@ -5,6 +5,7 @@ public partial class IncreaseCircleSize : Node2D {
 	
 	public Button _button;
 	public AnimationPlayer _closeAnim;
+	public Node sceneToLoad;
 	[Export] public ShaderMaterial _shader;
 	//public float shaderCopy;
 	public override void _Ready() {
@@ -13,10 +14,25 @@ public partial class IncreaseCircleSize : Node2D {
 
 		_closeAnim = GetNode<AnimationPlayer>("ColorRect/AnimationPlayer");
 		_closeAnim.Play("Close");
+		
+	}
+	
+	public override void _Process(double delta)
+	{
+		
 	}
 	
 	public void UpdateCircleSize() {
 		var time =_closeAnim.CurrentAnimationPosition;
 		_closeAnim.Seek(time-1);
 	}
+	
+	//Go to Game over screen if Animation hits zero
+	private void _on_animation_player_animation_finished(StringName anim_name)
+	{
+		GD.Print("Deeeeead");
+		GetTree().ChangeSceneToFile("res://Scenes and Scripts/game_over.tscn");
+
+	}
 }
+
