53d714487d50b9692ba1f497ebfd1605fca31cd0
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-20 12:53:01 +0200

    added lots of enemy und so

---
+++ Scenes and Scripts/enemy.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+var player_inside_down_area = false
+var player_inside_left_area = false
+var player_inside_up_area = false
+var player_inside_right_area = false
+
+var enemy_direction = "none"
+
+var round = 0
+
+func _process(delta):
+	if player_inside_down_area and enemy_direction == "down":
+		get_tree().change_scene_to_file("res://Scenes and Scripts/game_over.tscn")
+
+
+func _on_timer_timeout():
+
+	$Sprite2D.rotate(1.5707963267949) # 90° sind 1.5707963267949 rad
+	print(enemy_direction)
+	
+	if round == 0 and Global.should_start_enemy_direction_switch:
+		enemy_direction = "right"
+		round += 1
+	elif round == 1 and Global.should_start_enemy_direction_switch:
+		enemy_direction = "down"
+		round += 1
+	elif round == 2 and Global.should_start_enemy_direction_switch:
+		enemy_direction = "left"
+		round += 1
+	elif round == 3 and Global.should_start_enemy_direction_switch:
+		enemy_direction = "up"
+		round = 0
+	
+
+	
+
+
+
+
+
+
+func _on_down_scan_body_entered(body):
+	if body.get_name() == "Player":
+		player_inside_down_area = true
+func _on_down_scan_body_exited(body):
+	if body.get_name() == "Player":
+		player_inside_down_area = false
--- Scenes and Scripts/level.gd
+++ Scenes and Scripts/level.gd
@@ -2,22 +2,32 @@ extends Node2D
 
 var tiles1 = preload("res://Scenes and Scripts/Tiles/tile_1.tscn")
 var tiles2 = preload("res://Scenes and Scripts/Tiles/tile_2.tscn")
+var tiles3 = preload("res://Scenes and Scripts/Tiles/tile_3.tscn")
 var last_tile_position = Vector2(0, 0)
-
+var should_spawn_tiles = true
 
 func _process(delta):
-	randomize()
-	var tiles = [tiles1, tiles2]
-	var help = tiles[randi()% tiles.size()]
-	var tile = help.instantiate()
-	add_child(tile)
-	tile.position = last_tile_position
-	last_tile_position += Vector2(1920, 0)
+	if should_spawn_tiles:
+		randomize()
+		var tiles = [tiles1, tiles2, tiles3]
+		var help = tiles[randi()% tiles.size()]
+		var tile = help.instantiate()
+		add_child(tile)
+		tile.position = last_tile_position
+		last_tile_position += Vector2(1920, 0)
 	
-	
-	
-	if last_tile_position.x >= 57600:
-		last_tile_position.x = 0
-		last_tile_position += Vector2(0, 1080)
+		if last_tile_position.x >= 19200:
+			last_tile_position.x = 0
+			last_tile_position += Vector2(0, 1080)
+		
+		if last_tile_position.y == 10800:
+			should_spawn_tiles = false
 
-	
+
+
+
+
+
+
+func _on_timer_timeout():
+	Global.should_start_enemy_direction_switch = true
