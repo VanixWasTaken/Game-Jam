1c2a76d82be50caed171e2cbe55bc9aab5c9dbf3
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-20 09:14:54 +0200

    Revert "spiral spawn test"
    This reverts commit e0fd1e3ec0df558232b5fb5e18ba952977e28da5.

--- Scenes and Scripts/level.gd
+++ Scenes and Scripts/level.gd
@@ -1,44 +1,23 @@
 extends Node2D
 
-var cross_tiles1 = preload("res://Scenes and Scripts/Tiles/tile_1.tscn")
-var cross_tiles2 = preload("res://Scenes and Scripts/Tiles/tile_2.tscn")
-var cross_tiles3 = preload("res://Scenes and Scripts/Tiles/tile_3.tscn")
+var tiles1 = preload("res://Scenes and Scripts/Tiles/tile_1.tscn")
+var tiles2 = preload("res://Scenes and Scripts/Tiles/tile_2.tscn")
 var last_tile_position = Vector2(0, 0)
-var tile_round = 0
 
 
 func _process(delta):
 	randomize()
-	var cross_tiles = [cross_tiles1, cross_tiles2, cross_tiles3]
-	var help = cross_tiles[randi()% cross_tiles.size()]
-	var cross_tile = help.instantiate()
-	add_child(cross_tile)
+	var tiles = [tiles1, tiles2]
+	var help = tiles[randi()% tiles.size()]
+	var tile = help.instantiate()
+	add_child(tile)
+	tile.position = last_tile_position
+	last_tile_position += Vector2(1920, 0)
 	
 	
-	cross_tile.position = last_tile_position
 	
-	if tile_round == 5:
-		last_tile_position.y -= 1080
-		tile_round += 1
-	
-	if tile_round == 4:
-		last_tile_position.y -= 1080
-		tile_round += 1
-	
-	if tile_round == 3:
-		last_tile_position.x -= 1920
-		tile_round += 1
-	
-	if tile_round == 2:
-		last_tile_position.x -= 1920
-		tile_round += 1
-	
-	if tile_round == 1:
-		last_tile_position.y += 1080
-		tile_round += 1
-	
-	if tile_round == 0:
-		last_tile_position.x += 1920
-		tile_round += 1
-		
+	if last_tile_position.x >= 57600:
+		last_tile_position.x = 0
+		last_tile_position += Vector2(0, 1080)
+
 	
