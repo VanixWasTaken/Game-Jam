13f6101b8874258023894d0e563e721e40cd64fe
VanixWasTaken <136642594+VanixWasTaken@users.noreply.github.com>
2024-04-20 13:24:16 +0200

    alkjshdclakcvlacbfsa

--- Scenes and Scripts/enemy.gd
+++ Scenes and Scripts/enemy.gd
@@ -12,6 +12,16 @@ var round = 0
 func _process(delta):
 	if player_inside_down_area and enemy_direction == "down":
 		get_tree().change_scene_to_file("res://Scenes and Scripts/game_over.tscn")
+	if player_inside_left_area and enemy_direction == "left":
+		get_tree().change_scene_to_file("res://Scenes and Scripts/game_over.tscn")
+	if player_inside_up_area and enemy_direction == "up":
+		get_tree().change_scene_to_file("res://Scenes and Scripts/game_over.tscn")
+	if player_inside_right_area and enemy_direction == "right":
+		get_tree().change_scene_to_file("res://Scenes and Scripts/game_over.tscn")
+
+
+
+
 
 
 func _on_timer_timeout():
@@ -46,3 +56,27 @@ func _on_down_scan_body_entered(body):
 func _on_down_scan_body_exited(body):
 	if body.get_name() == "Player":
 		player_inside_down_area = false
+
+
+func _on_left_scan_body_entered(body):
+	if body.get_name() == "Player":
+		player_inside_left_area = true
+func _on_left_scan_body_exited(body):
+	if body.get_name() == "Player":
+		player_inside_left_area = false
+
+
+func _on_up_scan_body_entered(body):
+	if body.get_name() == "Player":
+		player_inside_up_area = true
+func _on_up_scan_body_exited(body):
+	if body.get_name() == "Player":
+		player_inside_up_area = false
+
+
+func _on_right_scan_body_entered(body):
+	if body.get_name() == "Player":
+		player_inside_right_area = true
+func _on_right_scan_body_exited(body):
+	if body.get_name() == "Player":
+		player_inside_right_area = false
